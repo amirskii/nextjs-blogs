7045282d0987d2788a24bc61045b0a525cf8d474
rti-mobile <amir.priv@gmail.com>
2021-08-18 15:26:31 +0600

    remove i18n from next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   // https://github.com/vercel/next.js/issues/21079
   // Remove the workaround the issue is fixed
   images: {
